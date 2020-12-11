b1b8e982b7fab950b1f71605fa2cc07a63117ef9
LD <chiyc88@gmail.com>
2020-12-11 12:11:53 +0800

    Floor can now continuously moving. - 28:14 watched

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ pygame.init()
 screen = pygame.display.set_mode((576, 800))
 clock = pygame.time.Clock()
 
-bg_surface = pygame.image.load('assets/background-day.png')
-bg_surface = pygame.transform.scale2x(bg_surface)
+bg_surface = pygame.transform.scale2x(pygame.image.load('assets/background-day.png').convert())
+floor_surface = pygame.transform.scale2x(pygame.image.load('assets/base.png').convert())
+floor_x = 0
+
+def moving_floor():
+    screen.blit(floor_surface,(floor_x, 675))
+    screen.blit(floor_surface,(floor_x + 576, 675))
 
 while True:
     for event in pygame.event.get():
@@ -13,7 +18,11 @@ while True:
             pygame.quit()
             sys.exit()
 
-    screen.blit(bg_surface,(0, 0))
+    screen.blit(bg_surface, (0, -150))
+    moving_floor()
+    floor_x = floor_x - 1
+    if floor_x == -576:
+        floor_x = 0
 
     pygame.display.update()
     clock.tick(120)
