5ba2c459b417830561390db28f3b556221b2a2e6
LD <chiyc88@gmail.com>
2021-01-08 00:02:27 +0800

    Add sound Remake scoring system Remove flap_sound Clear redundant pipes Add more comments Optimize code - Finished

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
-import pygame
 from random import randrange
+
+import pygame
 
 # Starting
 pygame.init()
@@ -8,7 +9,7 @@ screen = pygame.display.set_mode((576, 800))
 clock = pygame.time.Clock()
 game_font = pygame.font.Font('FlappyBirdFont.ttf', 50)
 
-# Importing Images
+# Importing Images and Sounds
 bg_surface = pygame.transform.scale2x(pygame.image.load('assets/background-day.png').convert())
 floor_surface = pygame.transform.scale2x(pygame.image.load('assets/base.png').convert())
 pipe_surface = pygame.transform.scale2x(pygame.image.load('assets/pipe-green.png').convert())
@@ -17,6 +18,9 @@ bird_midflap = pygame.transform.scale2x(pygame.image.load("assets/bluebird-midfl
 bird_upflap = pygame.transform.scale2x(pygame.image.load("assets/bluebird-upflap.png").convert_alpha())
 game_over_surface = pygame.transform.scale2x(pygame.image.load('assets/gameover.png').convert_alpha())
 get_ready_surface = pygame.transform.scale2x(pygame.image.load('assets/message.png').convert_alpha())
+hit_sound = pygame.mixer.Sound('sounds/sfx_hit.wav')
+score_sound = pygame.mixer.Sound('sounds/sfx_point.wav')
+death_sound = pygame.mixer.Sound('sounds/sfx_die.wav')
 
 # Bird's Animation
 bird_frames = [bird_downflap, bird_midflap, bird_upflap]
@@ -31,9 +35,11 @@ pygame.time.set_timer(BIRDFLAP, 200)
 floor_x = 0
 gravity = 0.25
 bird_movement = 0
-game_start = True
+game_start = False
 score = 0
+can_score = True
 highest_score = 0
+death_count = 0
 
 # Spawn Pipes
 pipe_list = []
@@ -61,7 +67,8 @@ def create_pipe():
 def move_pipes(pipes):
     for pipe in pipes:
         pipe.centerx -= 4
-    return pipes
+    visible_pipes = [pipe for pipe in pipes if pipe.right > -50]
+    return visible_pipes
 
 
 def draw_pipes(pipes):
@@ -76,8 +83,10 @@ def draw_pipes(pipes):
 def check_collisions(pipes):
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
+            hit_sound.play()
             return False
     if bird_rect.top <= -100 or bird_rect.bottom >= 750:
+        death_sound.play()
         return False
     else:
         return True
@@ -94,12 +103,24 @@ def bird_animation():
     return new_bird, new_bird_rect
 
 
+def score_check():
+    global score, can_score
+    if pipe_list:
+        for pipe in pipe_list:
+            if 98 < pipe.centerx < 102 and can_score:
+                score += 1
+                score_sound.play()
+                can_score = False
+            if pipe.centerx < 0:
+                can_score = True
+
+
 def score_display(game_status):
     if game_status == "mid_game":
         score_surface = game_font.render(str(int(score)), True, (255, 255, 255))
         score_rect = score_surface.get_rect(center=(288, 100))
         screen.blit(score_surface, score_rect)
-    if game_status == "game_over":
+    if game_status == "game_over" and death_count > 0:
         score_surface = game_font.render(f'Score: {int(score)}', True, (255, 255, 255))
         score_rect = score_surface.get_rect(center=(150, 175))
         screen.blit(score_surface, score_rect)
@@ -125,9 +146,13 @@ while True:
 
         # Space button triggered event
         if event.type == pygame.KEYDOWN:
+
+            # Space event during game
             if event.key == pygame.K_SPACE and game_start == True:
                 bird_movement = 0
                 bird_movement -= 10
+
+            # Space event when gameover
             if event.key == pygame.K_SPACE and game_start == False:
                 pipe_list.clear()
                 bird_rect.center = (100, 400)
@@ -135,6 +160,8 @@ while True:
                 game_start = True
                 bird_movement -= 10
                 score = 0
+                can_score = True
+                death_count += 1
 
         # Spawn pipes
         if event.type == SPAWNPIPE:
@@ -174,10 +201,12 @@ while True:
         game_start = check_collisions(pipe_list)
 
         # Display Score
-        score += 0.01
+        score_check()
         score_display("mid_game")
     else:
-        screen.blit(game_over_surface, game_over_rect)
+        # Gameover Screen Display
+        if death_count > 0:
+            screen.blit(game_over_surface, game_over_rect)
         screen.blit(get_ready_surface, get_ready_rect)
         highest_score = highest_score_update(score, highest_score)
         score_display("game_over")
